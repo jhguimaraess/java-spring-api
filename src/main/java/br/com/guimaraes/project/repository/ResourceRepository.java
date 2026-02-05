f05239253ce221d39d9c0bad7186b9ed102da977
joao guimaraes <guimaraesjoao101@gmail.com>
2026-02-05 11:39:11 -0300

    feat: add resource repository

---
+++ src/main/java/br/com/guimaraes/project/repository/ResourceRepository.java
@@ -0,0 +1,9 @@
+package br.com.guimaraes.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.guimaraes.project.entity.ResourceEntity;
+
+public interface ResourceRepository extends JpaRepository<ResourceEntity, Long>{
+
+}
