67e26aa3399091a3a2c8f4ddc9cf263f3cbb2e93
joao guimaraes <guimaraesjoao101@gmail.com>
2026-02-05 12:09:02 -0300

    feat: implement resource service CRUD operations

--- src/main/java/br/com/guimaraes/project/service/ResourceService.java
+++ src/main/java/br/com/guimaraes/project/service/ResourceService.java
@@ -1,7 +1,11 @@
 package br.com.guimaraes.project.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.guimaraes.project.dto.ResourceDTO;
+import br.com.guimaraes.project.entity.ResourceEntity;
 import br.com.guimaraes.project.repository.ResourceRepository;
 
 public class ResourceService {
@@ -9,5 +13,28 @@ public class ResourceService {
 	@Autowired
 	private ResourceRepository resourceRepository;
 	
+	public List<ResourceDTO> listAll(){
+		List<ResourceEntity> resource = resourceRepository.findAll();
+		return resource.stream().map(ResourceDTO::new).toList();
+	}
+	
+	public void insert(ResourceDTO resource) {
+		ResourceEntity resourceEntity = new ResourceEntity(resource);
+		resourceRepository.save(resourceEntity);
+	}
+	
+	public ResourceDTO update(ResourceDTO resource) {
+		ResourceEntity resourceEntity = new ResourceEntity(resource);
+		return new ResourceDTO(resourceRepository.save(resourceEntity));
+	}
+	
+	public void delete(Long id) {
+		ResourceEntity resource = resourceRepository.findById(id).get();
+		resourceRepository.delete(resource);
+	}
+	
+	public ResourceDTO searchForId(Long id) {
+		return new ResourceDTO(resourceRepository.findById(id).get());
+	}
 	
 }
