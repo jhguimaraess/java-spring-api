ad63a8e7c98b3835f792344ea89ddcf0c9caf7e4
joao guimaraes <guimaraesjoao101@gmail.com>
2026-02-10 22:53:37 -0300

    feat: extend authentication structure

--- src/main/java/br/com/guimaraes/project/controller/AuthController.java
+++ src/main/java/br/com/guimaraes/project/controller/AuthController.java
@@ -1,10 +1,19 @@
 package br.com.guimaraes.project.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.guimaraes.project.dto.AuthenticationDTO;
+
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthController {
+	
+	@PostMapping(value = "/login")
+	public ResponseEntity<?> login(@RequestBody AuthenticationDTO authDto)
 
 }
---
+++ src/main/java/br/com/guimaraes/project/service/AuthService.java
@@ -0,0 +1,41 @@
+package br.com.guimaraes.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.com.guimaraes.project.dto.AuthenticationDTO;
+import br.com.guimaraes.project.security.jwt.JwtUtils;
+
+@Service
+public class AuthService {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+
+	public AcessDTO login(AuthenticationDTO authDto) {
+		
+		try {
+			//Cria mecanismo de credencial para o spring
+			UsernamePasswordAuthenticationToken userAuth = new UsernamePasswordAuthenticationToken(authDto.getUsername(), authDto.getPassword());
+		
+			//prepara mecanismo para autenticacao
+			Authentication authentication = authenticationManager.authenticate(userAuth);
+		
+			//busca usuario logado
+			UserDetailsImpl userAuthenticate = (UserDetailsImpl)authentication.getPrincipal();
+			
+			String token = jwtUtils.generateTokenFromUserDetailsImpl(userAuthenticate);
+			
+		}catch(BadCredentialsException e) {
+			//TODO login ou senha invalida
+		}
+	}
+	
+}
